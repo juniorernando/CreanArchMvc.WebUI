9024398540bc2f0a9785ced9ef7b54843960c11c
Junior Fernando <junior_618@hotmail.com>
2023-09-23 23:18:05 -0300

    Atualizado

--- CreanArchMvc.WebUI/Program.cs
+++ CreanArchMvc.WebUI/Program.cs
@@ -1,10 +1,16 @@
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
